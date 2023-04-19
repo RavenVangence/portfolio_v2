79f6de273a110c65780c2cb1f0f942fcede0cfdf
Raven Munonela <ravenmunonela@gmail.com>
2023-04-19 10:38:05 +0200

    animated banner

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -19,13 +19,13 @@ function NavbarAndDashboard() {
                 </ul>
             </nav>
             <div className='dashboard-container'>
-                <div className='dashboard-par'>
-                    <p>
+                <div className='dashboard-left-container'>
+                    <p id='dashboard-par'>
                         Welcome to my portfolio! My name is Ndivhuwo Lodrick Munonela, I am a full-stack MERN developer, passionate about creating dynamic web applications that deliver seamless user experiences. Browse my work and let's bring your project to life!
                     </p>
                     <div className='dashboard-icon-container'>
-                        <FaReact id='react-icon' className='dashboard-icon'/>
                         <FaHtml5 id='html-icon' className='dashboard-icon'/>
+                        <FaReact id='react-icon' className='dashboard-icon'/>
                         <FaGit id='git-icon' className='dashboard-icon'/>
                         <TbBrandTypescript id='typescript-icon' className='dashboard-icon'/>
                         <SiExpress id='express-icon' className='dashboard-icon'/>
@@ -35,6 +35,10 @@ function NavbarAndDashboard() {
                         <SiRedux id='redux-icon' className='dashboard-icon'/>
                         <IoLogoJavascript id='javascript-icon' className='dashboard-icon'/>
                         <FiFramer id='framer-icon' className='dashboard-icon'/>
+                    </div>
+                    <div className='dashboard-btn-container'>
+                        <button id='hire-me-btn' className='dashboard-btn'>Hire Me</button>
+                        <button id='resume-btn' className='dashboard-btn'>Resume</button>
                     </div>
                 </div>
                 <figure className='profile-img'>
